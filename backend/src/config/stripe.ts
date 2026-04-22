51d68a1cc67154a304e249a5617f72557768e2e3
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-04-21 21:13:40 -0300

    chore: update package.json to include prisma generation in build script and add prisma as a dependency; update Stripe API version in configuration

--- backend/src/config/stripe.ts
+++ backend/src/config/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe'
 import { env } from './env'
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: '2026-03-25.dahlia',
+    apiVersion: '2025-08-27.basil',
     typescript: true,
     appInfo: {
         name: 'Petrica',
